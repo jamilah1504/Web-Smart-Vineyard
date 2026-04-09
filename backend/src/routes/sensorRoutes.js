7eee64b9c17c0aeeba5b9b6a8f5958b32ffa92f3
Alya Maratun Jamilah <alyamaratun12356@gmail.com>
2026-04-09 14:07:20 +0700

    "sensor dan telegram"

--- backend/src/routes/sensorRoutes.js
+++ backend/src/routes/sensorRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { receiveSensorData, getLatestSensorData } = require('../controllers/sensorController');
 const { protect } = require('../middleware/authMiddleware');
+const sendTelegram = require('../utils/telegram');
 
 const router = express.Router();
 
@@ -9,5 +10,13 @@ router.post('/data', receiveSensorData);
 
 // Rute GET untuk React Dashboard (Di-protect karena hanya user login yang boleh lihat data)
 router.get('/data/:perangkat_id', protect, getLatestSensorData);
+router.get('/test-tele', async (req, res) => {
+    try {
+        await sendTelegram("🚀 *TES KONEKSI AETERA*\nBot berhasil terhubung ke server!");
+        res.send("Pesan tes terkirim ke Telegram!");
+    } catch (err) {
+        res.status(500).send("Gagal: " + err.message);
+    }
+});
 
 module.exports = router;
