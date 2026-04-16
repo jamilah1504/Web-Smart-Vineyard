dbe1f7703e4db4d73c6d0fee81c9f29e07142eeb
Alya Maratun Jamilah <alyamaratun12356@gmail.com>
2026-04-16 21:36:20 +0700

    fitur tandon

--- backend/src/routes/tandonRoutes.js
+++ backend/src/routes/tandonRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getLatestWaterLevel, controlPump } = require('../controllers/tandonController'); 
 const { protect } = require('../middleware/authMiddleware');
 
-router.get('/khusus-tandon/:perangkat_id', protect, getLatestWaterLevel);
+router.get('/baca-air-tandon/:perangkat_id', getLatestWaterLevel);
 router.post('/control-pump', protect, controlPump);
 
 module.exports = router;
